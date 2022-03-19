248bba5cfe24cdae6ef26a415ab619cb4868a4e4
Geromet <gerome.tokic@gmail.com>
2022-03-19 15:37:11 +0100

    Add support for country files

--- Hack/Presets/Country.cs
+++ Hack/Presets/Country.cs
@@ -8,12 +8,13 @@ namespace Eu4HackGUI.Hack.Presets
 {
     internal class Country
     {
+        private static string fileName = "Hol - Holland.txt";
         private static string backupDir = "Files/backup/history/countries";
         private static string sourceDir = "Files/history/countries";
         private static string destinationDir = "C:/Program Files (x86)/Steam/steamapps/common/Europa Universalis IV/history/countries";
         private static string searchString = "Dynasty Colors Initialized";
-        private static int copyDelay = 0;
-        public static void Add(string fileName = "*.txt")
+        private static int copyDelay = 100;
+        public static void Add()
         {
             Hack.AddType(fileName, backupDir, sourceDir, destinationDir, searchString, copyDelay);
         }
