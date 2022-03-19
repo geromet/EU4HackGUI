80f6df1214082742083a70fc403c023a2bed8c8d
Geromet <gerome.tokic@gmail.com>
2022-03-19 16:43:06 +0100

    Publish alpha version with preset for ideas, country and province

---
+++ publish/Hack/Presets/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eu4HackGUI.Hack.Presets
+{
+    internal class Country
+    {
+        private static string fileName = "Hol - Holland.txt";
+        private static string backupDir = "Files/backup/history/countries";
+        private static string sourceDir = "Files/history/countries";
+        private static string destinationDir = "C:/Program Files (x86)/Steam/steamapps/common/Europa Universalis IV/history/countries";
+        private static string searchString = "LeaderPersonalities loaded";
+        private static int copyDelay = 0;
+        public static void Add()
+        {
+            Hack.AddType(fileName, backupDir, sourceDir, destinationDir, searchString, copyDelay);
+        }
+    }
+}
