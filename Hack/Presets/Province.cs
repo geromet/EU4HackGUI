3ea0ee1d6abfba88fcc73d4f6d423d485749ebc5
Geromet <gerome.tokic@gmail.com>
2022-03-20 14:26:08 +0100

    E

--- Hack/Presets/Province.cs
+++ Hack/Presets/Province.cs
@@ -11,7 +11,7 @@ namespace Eu4HackGUI.Hack.Presets
         private static string fileName = "97 - Holland.txt";
         private static string backupDir = "Files/backup/history/provinces";
         private static string sourceDir = "Files/history/provinces";
-        private static string destinationDir = "C:/Program Files (x86)/Steam/steamapps/common/Europa Universalis IV/history/provinces";
+        private static string destinationDir = "e:/Steam/steamapps/common/Europa Universalis IV/history/provinces";
         private static string searchString = "LeaderPersonalities loaded";
         private static int copyDelay = 100;
         public static void Add()
